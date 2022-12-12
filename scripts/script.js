06cb269afe3878916cb33358985002141ed48c5e
Aaron Gomez <a13g24@gmail.com>
2022-12-12 11:12:52 -0700

    Finished carousel

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+let slidesContainer = document.getElementById("slider-container");
+let slide = document.querySelector(".slider__slide");
+let prevButton = document.getElementById("slider-arrow-prev");
+let nextButton = document.getElementById("slider-arrow-next");
+
+nextButton.addEventListener("click", () => {
+    let slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+    let slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+});
