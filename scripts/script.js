9fecde876ab67833e14ea4c459135bb13e5508c4
Aaron Gomez <a13g24@gmail.com>
2022-12-12 18:56:40 -0700

    Fixed button focus problem;

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,14 @@ let slide = document.querySelector(".slider__slide");
 let prevButton = document.getElementById("slider-arrow-prev");
 let nextButton = document.getElementById("slider-arrow-next");
 
-nextButton.addEventListener("click", () => {
+nextButton.addEventListener("click", (e) => {
     let slideWidth = slide.clientWidth;
     slidesContainer.scrollLeft += slideWidth;
 
     // FIXME: Btn must go transparent again after click
 });
 
-prevButton.addEventListener("click", () => {
+prevButton.addEventListener("click", (e) => {
     let slideWidth = slide.clientWidth;
     slidesContainer.scrollLeft -= slideWidth;
 
